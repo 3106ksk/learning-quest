636865b000e841d0862d2a70f7a56b7bcbdb364a
Satoru kamiko <kiritaro973@gmail.com>
2026-07-05 12:24:01 +0900

    feat: #26 ユーザーの新規登録処理を追加
    変更点
     - createアクションの追加

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,6 +1,23 @@
 class SignUpsController < ApplicationController
-  allow_unauthenticated_access only: %i[show]
+  allow_unauthenticated_access
   def show
     @user = User.new
   end
+
+
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save
+        start_new_session_for(@user)
+        redirect_to root_path, notice: "Signed up successfully"
+    else
+        flash.now[:alert] = "Failed to sign up"
+        render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def sign_up_params
+      params.expect(user: [ :name, :email_address, :password, :password_confirmation ])
+    end
 end

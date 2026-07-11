30bf83a848fb45fb664588c1a59790d8cd7fa152
Satoru kamiko <kiritaro973@gmail.com>
2026-07-11 09:18:45 +0900

    feat: #30 LP用のStaticPagesとルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   resource :session
   resources :passwords, param: :token
   resource :sign_up
-  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

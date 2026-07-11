4147b7f0fc678ab53f12c5d84a8661d6812f3a48
Satoru kamiko <kiritaro973@gmail.com>
2026-07-11 10:41:39 +0900

    fix: #30 テストのURLパスの修正

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  setup { sign_in_as users(:one) }
+
   test "should get index" do
-    get home_index_url
+    get home_url
     assert_response :success
   end
 end

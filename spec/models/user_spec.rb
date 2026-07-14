ec7e15defce867ecbb65e1566aa11b1c39467c5b
Satoru kamiko <kiritaro973@gmail.com>
2026-07-15 08:44:57 +0900

    test: #106 Userの名前バリデーションのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,31 @@ RSpec.describe User, type: :model do
     expect(user).to be_persisted
     expect(user).to be_valid
   end
+
+  describe "名前のバリデーション" do
+    context "名前がある場合" do
+      it "有効である" do
+        user = build(:user, name: "テストユーザー")
+
+        expect(user).to be_valid
+      end
+    end
+
+    context "名前がない場合は無効となりエラーメッセージが表示される" do
+      it "無効である" do
+        user = build(:user, name: nil)
+
+        expect(user).not_to be_valid
+        expect(user.errors[:name]).to be_present
+      end
+    end
+
+    context "名前が51文字の場合" do
+      it "無効である" do
+        user = build(:user, name: "あ" * 51)
+
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end

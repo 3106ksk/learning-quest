a6aa16d74d63379d5843d74d6874edcf2156465c
Satoru kamiko <kiritaro973@gmail.com>
2026-07-16 16:59:08 +0900

    feat: #39 カラムの追加
    背景
    - マイグレーション作成時に 学習を開始した時間を記録するためのカラムが追記できていなかった
    
    変更点
    - started_atカラムを追加してマイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_16_000000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_16_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_16_000000) do
     t.string "activity", limit: 100
     t.datetime "created_at", null: false
     t.integer "planned_minutes", null: false
+    t.datetime "started_at", null: false
     t.string "status", default: "running", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false

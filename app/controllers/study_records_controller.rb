1c9ceb9a53847136ca1a12b4f2458c77b8c9f983
Satoru kamiko <kiritaro973@gmail.com>
2026-07-16 18:32:38 +0900

    feat: #39 フォームから送信されたパラメーターを保存する処理の追加
    変更点
    - createアクションの追加

--- app/controllers/study_records_controller.rb
+++ app/controllers/study_records_controller.rb
@@ -7,11 +7,26 @@ class StudyRecordsController < ApplicationController
   end
 
   def create
+    @study_record = Current.user.study_records.build(
+      study_record_params.merge(started_at: Time.current)
+    )
+
+    if @study_record.save
+      redirect_to home_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def study_record_params
+    params.require(:study_record).permit(:planned_minutes)
   end
 end

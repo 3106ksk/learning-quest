60b8eb8847306b1f492084feb54b994469dd46b5
Satoru kamiko <kiritaro973@gmail.com>
2026-07-16 15:24:05 +0900

    feat: #39 学習レコード作成フォームの追記
    背景
    学習フォーム機能が表示されるかをチェックするために、ルート、コントローラー、ビューを作成
    
    変更点
    - resources: study_records追加
    - /views/study_records/new.html.erbの作成
    - /study_records_controller.rb newアクション作成

---
+++ app/controllers/study_records_controller.rb
@@ -0,0 +1,17 @@
+class StudyRecordsController < ApplicationController
+  def index
+  end
+
+  def new
+    @study_record = StudyRecord.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end

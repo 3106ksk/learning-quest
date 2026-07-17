b4ab1091acc2d875d8cef87d969f31fe669f95b9
Satoru kamiko <kiritaro973@gmail.com>
2026-07-17 10:01:34 +0900

    feat: #39 学習時間を選択式フォームに変更
    変更点
    - 選択のデータルールをモデルに集約

--- app/models/study_record.rb
+++ app/models/study_record.rb
@@ -1,4 +1,6 @@
   class StudyRecord < ApplicationRecord
+    ALLOWED_PLANNED_MINUTES = [ 5, 15, 25, 50 ].freeze
+
     belongs_to :user
 
     enum :status, {
@@ -8,7 +10,7 @@
       awaiting_evaluation: "awaiting_evaluation"
     }, validate: true
 
-    validates :planned_minutes, presence: true, inclusion: { in: [5, 15, 25, 50] }
+    validates :planned_minutes, presence: true, inclusion: { in: ALLOWED_PLANNED_MINUTES }
     validates :activity, length: { maximum: 100 }, allow_blank: true
     validates :started_at, presence: true
     validates :user_id, uniqueness: {

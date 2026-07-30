8dc73d11e63a9d7433e8c420e3ef08d3a6cac7d8
Satoru kamiko <kiritaro973@gmail.com>
2026-07-30 17:38:14 +0900

    feat: #52 評価入力フォーム表示用のコントローラーアクションを追加
    変更点
    - 評価フォーム表示のためのnewアクション追加
    - scopeを利用してorder(:position)を:display_orderとしてスコープ化

---
+++ app/controllers/evaluations_controller.rb
@@ -0,0 +1,22 @@
+class EvaluationsController < ApplicationController
+  before_action :set_study_record
+  before_action :ensure_awaiting_evaluation, only: :new
+
+  def new
+    @evaluation = @study_record.build_evaluation
+    @focus_options = FocusOption.display_order
+    @challenge_options = ChallengeOption.display_order
+  end
+
+  private
+
+  def set_study_record
+    @study_record = Current.user.study_records.find(params[:study_record_id])
+  end
+
+  def ensure_awaiting_evaluation
+    return if @study_record.awaiting_evaluation?
+
+    redirect_to @study_record, status: :see_other
+  end
+end

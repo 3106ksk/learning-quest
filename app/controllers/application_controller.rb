d1cf86b688a4f7333b65b1d8ce29d51e714e1ee3
Satoru kamiko <kiritaro973@gmail.com>
2026-09-05 12:46:24 +0900

    fix: #192 ヘッダーの認証リンクをDeviseのルートへ変更
    背景
    LP画面からアカウント作成画面、ログイン画面がDeviseの画面へ遷移するように修正すればDeviseのユーザー認証が正常に動作しているかを判断できると考えたのでヘッダーとLP画面の画面遷移の動線からDeviseのリソースを反映させる
    
    変更点
    - 共通ヘッダーのURLヘルパーをDeviseのURLヘルパーへ置き換え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
+  skip_before_action :require_authentication, if: :devise_controller?
+  
   add_flash_types :success, :danger
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.

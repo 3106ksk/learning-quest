c143e68ac09c8762ae41c9b21bf09801389b649b
Satoru kamiko <kiritaro973@gmail.com>
2026-09-05 14:04:28 +0900

    feat: #193 Deviseの認証ビューを追加しアカウント名登録に対応
    背景
    アカウント名を登録するためにDeviseのビューのカスタマイズと、Deviseのコントローラーで受け取るためのストロングパラメーターの受け取り許可の設定追加が必要だった
    
    変更点
    - Userスコープの登録・ログインビューを生成
    - Deviseのscoped_viewsを有効化
    - 新規登録時の許可パラメーターにaccount_nameを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < ActionController::Base
   
   add_flash_types :success, :danger
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [ :account_name ])
+    end
 end

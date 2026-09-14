3705d4ba4ad482a7bce6ac367ac8a688839aea94
Satoru kamiko <kiritaro973@gmail.com>
2026-09-14 19:18:29 +0900

    feat: #202 目標作成処理と成功/失敗メッセージを追加
    背景
    ブラウザから送信されたユーザーが作成した目標名を受け取りdbに保存する処理を追加。デフォルトではステータスがactiveで保存されて、complete状態でなければ新規目標作成はできない。
    
    変更点:
    - GoalsController#createを追加
    - 作成成功時に学習開始画面へ遷移する処理を追加
    - 成功時のフラッシュメッセージを追加
    - 作成失敗時のエラーメッセージを追加
    - 共通レイアウトで成功メッセージを表示する処理を追加

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,4 +2,21 @@ class GoalsController < ApplicationController
   def new
     @goal = current_user.goals.build
   end
+
+  def create
+    @goal = current_user.goals.build(goal_params)
+
+    if @goal.save
+      redirect_to new_study_record_path, success: t(".success"), status: :see_other
+    else
+      flash.now[:danger] = t(".danger")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def goal_params
+    params.expect(goal: [ :name ])
+  end
 end
